e7bfd02f1e537572a8397538fb24fee3a6d4d447
Matthew Forth <matty@studiorepublic.com>
2024-12-18 20:24:34 +0000

    initial commit

---
+++ nvim/lua/custom/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'V13Axel/neotest-pest',
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = { require 'neotest-pest' {
+        ignore_dirs = { 'node_modules', 'vendor' },
+      } },
+    }
+  end,
+}
