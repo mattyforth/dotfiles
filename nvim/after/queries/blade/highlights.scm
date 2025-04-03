b6b447eb6e7b2d156a4729e5a966b63dfc79709e
Matthew Forth <matty@studiorepublic.com>
2025-04-03 17:07:38 +0100

    chore: changed blade treesitter queries

--- nvim/after/queries/blade/highlights.scm
+++ nvim/after/queries/blade/highlights.scm
@@ -1,14 +1,23 @@
-(directive) @function
-(directive_start) @function
-(directive_end) @function
-; Changed the above to @tag instead, as per https://seankegel.com/neovim-for-php-and-laravel#heading-blade-highlights
-;(directive) @tag
-;(directive_start) @tag
-;(directive_end) @tag
+(directive) @tag
+(directive_start) @tag
+(directive_end) @tag
+(comment) @comment @spell
+[
+ (bracket_start)
+ (bracket_end)
+] @punctuation.bracket
 
-(comment) @comment
-((parameter) @include (#set! "priority" 110)) 
-((php_only) @include (#set! "priority" 110)) 
-((bracket_start) @function (#set! "priority" 120)) 
-((bracket_end) @function (#set! "priority" 120)) 
-(keyword) @function
+; (directive) @function
+; (directive_start) @function
+; (directive_end) @function
+; ; Changed the above to @tag instead, as per https://seankegel.com/neovim-for-php-and-laravel#heading-blade-highlights
+; ;(directive) @tag
+; ;(directive_start) @tag
+; ;(directive_end) @tag
+;
+; (comment) @comment
+; ((parameter) @include (#set! "priority" 110)) 
+; ((php_only) @include (#set! "priority" 110)) 
+; ((bracket_start) @function (#set! "priority" 120)) 
+; ((bracket_end) @function (#set! "priority" 120)) 
+ (keyword) @function
