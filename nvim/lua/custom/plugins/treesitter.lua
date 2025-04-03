7bc860fb38d8f8838d532b1c69fb784613e145e5
Matthew Forth <matty@studiorepublic.com>
2025-04-03 17:08:04 +0100

    feat: added comment.nvim

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
