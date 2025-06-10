da497036195b91cd7ffd33b3acecd376fa8393b0
Matthew Forth <matty@studiorepublic.com>
2025-06-10 15:30:02 +0100

    style: updated colourscheme

--- nvim/lua/mattyforth/plugins/colorscheme.lua
+++ nvim/lua/mattyforth/plugins/colorscheme.lua
@@ -1,17 +1,40 @@
 return {
-  'catppuccin/nvim',
-  name = 'catpuccin',
-  priority = 1000,
-  init = function()
-    require('catppuccin').setup {
-      flavour = 'auto',
+  {
+    'catppuccin/nvim',
+    name = 'catpuccin',
+    enabled = true,
+    priority = 1000,
+    init = function()
+      require('catppuccin').setup {
+        flavour = 'auto',
 
-      background = {
-        dark = 'mocha',
-        light = 'latte',
-      },
-    }
+        background = {
+          dark = 'frappe',
+          light = 'latte',
+        },
+      }
 
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    'sainnhe/sonokai',
+    lazy = false,
+    enabled = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      -- vim.g.sonokai_enable_italic = true
+      -- vim.cmd.colorscheme 'sonokai'
+    end,
+  },
+  {
+    'tanvirtin/monokai.nvim',
+    lazy = false,
+    enabled = false,
+    config = function()
+      require('monokai').setup{}
+    end
+  }
 }
