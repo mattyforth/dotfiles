096e37fe17f0061a09c6ebbc831be428de9c5481
Matthew Forth <matty@studiorepublic.com>
2026-01-08 17:03:57 +0000

    feat: added neotree file operations

--- nvim/lua/mattyforth/plugins/neo-tree.lua
+++ nvim/lua/mattyforth/plugins/neo-tree.lua
@@ -2,29 +2,44 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = true
-      },
-      window = {
-        position = 'float',
-        mappings = {
-          ['<leader>e'] = 'close_window',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    keys = {
+      { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+        window = {
+          position = 'float',
+          mappings = {
+            ['<leader>e'] = 'close_window',
+          },
         },
       },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Uncomment whichever supported plugin(s) you use
+      -- "nvim-tree/nvim-tree.lua",
+      'nvim-neo-tree/neo-tree.nvim',
+      -- "simonmclean/triptych.nvim"
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
   },
 }
