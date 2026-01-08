407333920f3f74ad6138c3d6e6b62f0f42863618
Matthew Forth <matty@studiorepublic.com>
2026-01-08 17:03:19 +0000

    chore: lazygit uses plenary for floating window

--- nvim/lua/mattyforth/plugins/lazygit.lua
+++ nvim/lua/mattyforth/plugins/lazygit.lua
@@ -1,20 +1,28 @@
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  'kdheepak/lazygit.nvim',
+  lazy = true,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+
+  -- options = {
+  --   lazygit_floating_window_use_plenary = true,
+  -- },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  },
+
+  init = function()
+    vim.g.lazygit_floating_window_use_plenary = 1
+  end,
 }
