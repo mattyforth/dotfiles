17412fbd13a691d65a5af2148e766303b7c64718
Matthew Forth <matty_cforth@outlook.com>
2026-05-09 13:45:08 +0100

    feat: added clone script

---
+++ clone.sh
@@ -0,0 +1,11 @@
+mkdir Code/Sites
+cd Code/Sites
+
+git clone git@github.com:studiorepublic/AM360-booking.git
+git clone git@github.com:studiorepublic/courtnav-v2.git
+git clone git@github.com:studiorepublic/nfwi.git
+git clone git@github.com:studiorepublic/courtnav-fl401-form-engine.git
+git clone git@github.com:studiorepublic/acuk-craft.git
+git clone git@github.com:studiorepublic/bliss-craft.git 
+git clone git@github.com:studiorepublic/acuk-api-actions.git
+
